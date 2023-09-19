6d430c21799db4204c54a54ec160ced5199878d8
AndersonPadovani <andersonpadovani@protonmail.com>
2023-09-19 20:18:12 -0300

    Criando classe de conexão com SQL

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+class DB{
+    public function __construct(){
+        try{
+            $varConfig = require_once "./config.php";
+
+            $dsn = sprintf("mysql:host=%s;dbname=%s", $varConfig["HOST_NAME"], $varConfig["DB_NAME"]);
+
+            $con = new PDO($dsn, $varConfig["DB_USER"], $varConfig["DB_PASS"]);
+            echo "conectado com sucesso";
+            return $con;
+        }catch(PDOException $error){
+            echo("Falha ao tentar conectar com o banco de dados!\n" . $error);
+        }     
+    }
+}
+new DB();
+?>
