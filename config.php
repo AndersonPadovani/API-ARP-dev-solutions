54a8e6df473ea240287a6d887fd865e3f8fbd77e
AndersonPadovani <andersonpadovani@protonmail.com>
2023-09-19 20:17:34 -0300

    Variaveis de configuração

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return[
+    //  Variaveis de configuração de conexao do banco de dados
+    "HOST_NAME" =>  "localhost",
+    "DB_USER"   =>  "root",
+    "DB_NAME"   =>  "arpDevSolutions",
+    "DB_PASS"   =>  "",
+
+    // Variavel de configuração do jwt
+    "JWT_PASS"  =>  "abee938a621d77cc63847621dffc74b91eecde1a"
+]
+?>
